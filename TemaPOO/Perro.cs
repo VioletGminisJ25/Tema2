8dda39f59a116b9ef69ce8b5225cdac3681a5b9b
VioletGminisJ25 <javierrodal25@gmail.com>
2024-09-20 10:35:32 +0200

    Agregar archivos de proyecto.

---
+++ TemaPOO/Perro.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaPOO
+{
+    class Perro
+    {
+
+        public string raza;
+        public string nombre;
+
+        private int edad;
+        // Nota: aunque lo veas así en este ejemplo,
+        // en C# NO haremos el set y el get de esta forma.
+        // La correcta se explica en un apartado posterior.
+
+        public int Edad { 
+            get 
+            {
+                return edad;
+            }
+            set
+            {
+                if (value < 0) { 
+                    value = 0;
+                }
+                this.edad = value;
+            }
+        }
+        //public int getEdad()
+        //{
+        //    return edad;
+        //}
+        //public void setEdad(int edad)
+        //{
+        //    this.edad = edad;
+        //}
+
+        public Perro()
+        {
+            this.Edad = 0;
+            this.raza = "";
+            this.nombre = "";
+        }
+    }
+    
+
+}
---
+++ TemaPOO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemaPOO;
+
+namespace TemaPOO
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Perro objPerro = new Perro();
+            objPerro.raza = "Mastín";
+            objPerro.nombre = "Laika";
+            objPerro.Edad = 5;
+            Console.WriteLine(objPerro.Edad);
+            Console.ReadLine();
+        }
+    }
+}
